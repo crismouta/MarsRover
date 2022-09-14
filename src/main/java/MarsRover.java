15748b7515a967850babf8d86bcfe1400b707343
CrisMouta <crismouta@gmail.com>
2022-09-14 11:03:35 +0200

    Green: test turn right

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,2 +1,12 @@
 public class MarsRover {
+    public String calculate(String command) {
+        if (command.equals("R")) {
+            return "E";
+        }
+        if (command.equals("RR"))
+        {
+            return "S";
+        }
+        return "W";
+    }
 }
--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -1,2 +1,21 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MarsRoverShould {
+    @ParameterizedTest
+    @CsvSource({
+            "R, E",
+            "RR, S",
+            "RRR, W",
+    })
+    public void turn_right(String input, String expectedOutput){
+        MarsRover rover = new MarsRover();
+
+        String result = rover.calculate(input);
+
+        assertEquals(expectedOutput, result);
+    }
 }
